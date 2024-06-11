adb60e05012dd2ebe3fd9d808d5653341137d8e7
casembrapati <casembrapa@casembrapa.org.br>
2024-06-11 12:04:37 -0300

    Update extrairChavePasse.js

--- src/extrairChavePasse.js
+++ src/extrairChavePasse.js
@@ -1,1 +1,7 @@
+// Extrai o valor do parâmetro "chavepasse" da URL
+const urlParams = new URLSearchParams(window.location.search);
+window.chavePasse = urlParams.get('chavePasse');
 
+// Exibe o valor na tela
+const resultadoDiv = document.getElementById('resultado');
+resultadoDiv.innerHTML = window.chavePasse;
